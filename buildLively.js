66e69c4dd33637fa888e533fe7cc6122ef588191
EliteAsian <lavasnakegaming@gmail.com>
2022-07-13 17:43:20 -0400

    Changed built zip name

--- buildLively.js
+++ buildLively.js
@@ -3,7 +3,7 @@ import archiver from "archiver";
 
 console.log("Starting build for Lively Wallpaper...");
 
-let output = createWriteStream("Matrix Wallpaper (Lively Wallpaper).zip");
+let output = createWriteStream("MatrixWallpaper_LivelyWallpaper.zip");
 
 let archive = archiver("zip");
 archive.on("error", function (err) {
@@ -20,6 +20,6 @@ archive.directory("./src_lively", false);
 archive.finalize();
 
 output.on("close", function () {
-	console.log(`Wrote ${archive.pointer()} bytes to "Matrix Wallpaper (Lively Wallpaper).zip"`);
+	console.log(`Wrote ${archive.pointer()} bytes to "MatrixWallpaper_LivelyWallpaper.zip"`);
 	console.log("You are ready to go!\n");
 });
